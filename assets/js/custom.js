656ab1ee80217b8949ccd40dfe0244392cc4eb46
Puneet Kohli <punkohl@gmail.com>
2018-04-24 17:34:47 -0500

    Create custom.js

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+$(document).ready ( function() {
+  $(".wrapper nav ul").children().each(function(index) {
+    if ($(this).hasClass("tag-h2") || $(this).hasClass("tag-h3")) {
+         $(this).remove()
+    }
+  }
+});
